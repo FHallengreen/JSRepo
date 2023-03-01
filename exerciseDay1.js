dcee4cf4a99f9a5f3bdcfa699290e6d2622f326d
Frederik Hallengreen <fred70p9@stud.kea.dk>
2023-03-01 09:46:26 +0100

    changes

--- exerciseDay1.js
+++ exerciseDay1.js
@@ -1,5 +1,5 @@
 //Observe: no return type, no type on parameters
-/* function add(n1, n2) {
+function add(n1, n2) {
   return n1 + n2;
 }
 
@@ -81,7 +81,7 @@ function createNamesList(names) {
 const names = ["Lars", "Peter", "Jan", "Ian"];
 const nameListHTML = createNamesList(names);
 console.log(nameListHTML);
- */
+
 const cars = [
   { id: 1, year: 1997, make: "Ford", model: "E350", price: 3000 },
   { id: 2, year: 1999, make: "Chevy", model: "Venture", price: 4900 },
@@ -90,13 +90,13 @@ const cars = [
   { id: 5, year: 2005, make: "Volvo", model: "V70", price: 4799 },
 ];
 
-/* const newCars = cars.filter(function (car) {
+const newCars = cars.filter(function (car) {
   return car.year > 1999 && car.make === "Volvo" && car.price < 5000;
 });
 
 newCars.forEach(function (car) {
   console.log(car);
-}); 
+});
 
 function myFilter(array, callback) {
   const filteredArr = [];
@@ -110,30 +110,28 @@ function myFilter(array, callback) {
 }
 
 function carFilter(car) {
- return car.year > 1999 && car.make === "Volvo" && car.price < 5000;
+  return car.year > 1999 && car.make === "Volvo" && car.price < 5000;
 }
 
 const filteredCars = myFilter(cars, carFilter);
 console.log(filteredCars);
-*/
 
-/* const msgPrinter = function(msg,delay){
-  setTimeout(() => console.log(msg),delay); //Observe an arrow-function
+const msgPrinter = function (msg, delay) {
+  setTimeout(() => console.log(msg), delay); //Observe an arrow-function
 };
 console.log("aaaaaaaaaa");
-msgPrinter ("bbbbbbbbbb",2000);
+msgPrinter("bbbbbbbbbb", 2000);
 console.log("dddddddddd");
-msgPrinter ("eeeeeeeeee",1000);
-console.log("ffffffffff"); */
+msgPrinter("eeeeeeeeee", 1000);
+console.log("ffffffffff");
 
 const employee = [
-  {id: 1, name: "Fred", age: 28, department: "ex pølle shipping mand"},
-  {id: 2, name: "Jeppe", age: 32, department: "Kongen af devops"}
-]
+  { id: 1, name: "Fred", age: 28, department: "ex pølle shipping mand" },
+  { id: 2, name: "Jeppe", age: 32, department: "Kongen af devops" },
+];
 
 delete employee.age;
 
-
-for(prop in employee){
-console.log(prop,employee[prop])
+for (prop in employee) {
+  console.log(prop, employee[prop]);
 }
